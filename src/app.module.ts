aaac2e838e48212009fd8eef3811501b9b442fcb
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2023-07-24 11:37:58 +0530

    doing global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static/dist/serve-static.module';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({
-  imports: [],
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+import { GlobalConfig } from 'config/global_config';
+
+@Module({  imports: [
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public'),
+  }),
+  JwtModule.register({
+    secret: GlobalConfig().JWT_SECRET_KEY,
+    signOptions: {},
+  }), //jwt implement
+  ConfigModule.forRoot({ isGlobal: true }),
+  MongooseModule.forRoot(process.env.DB_GULL_URL),
+  MongooseModule.forFeature([
+    
+    // { name: ModelNames.ROOT_CAUSES, schema: RootCausesSchema },
+   
+    
+  ]),
+  
+  // SalesReturnRequestStatusesModule,
+],
   controllers: [AppController],
   providers: [AppService],
 })
