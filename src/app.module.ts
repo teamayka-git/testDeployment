422145768223fb8cfe5a96a9a298f0e31d735801
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2023-07-24 13:27:40 +0530

    aaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-/*
 import { ServeStaticModule } from '@nestjs/serve-static/dist/serve-static.module';
+import { join } from 'path';
+/*
 import { JwtModule } from '@nestjs/jwt';
 
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { join } from 'path';
 import { GlobalConfig } from 'config/global_config';*/
 
 @Module({ 
   
-  /*
+  // /*
   imports: [
   ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'public'),
   }),
-  JwtModule.register({
-    secret: GlobalConfig().JWT_SECRET_KEY,
-    signOptions: {},
-  }), //jwt implement
-  ConfigModule.forRoot({ isGlobal: true }),
-  MongooseModule.forRoot(process.env.DB_GULL_URL),
-  MongooseModule.forFeature([
+  // JwtModule.register({
+  //   secret: GlobalConfig().JWT_SECRET_KEY,
+  //   signOptions: {},
+  // }), //jwt implement
+  // ConfigModule.forRoot({ isGlobal: true }),
+  // MongooseModule.forRoot(process.env.DB_GULL_URL),
+  // MongooseModule.forFeature([
     
-    // { name: ModelNames.ROOT_CAUSES, schema: RootCausesSchema },
+  //   // { name: ModelNames.ROOT_CAUSES, schema: RootCausesSchema },
    
     
-  ]),
+  // ]),
   
   // SalesReturnRequestStatusesModule,
 ],
-*/
+// */
 
-imports: [],
+// imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
